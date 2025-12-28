0e48620d5d8eaeceb400ead38a2170189539f79c
Vinay <vivinaykp@gmail.com>
2025-12-27 18:48:27 -0800

    Adding workflow

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
   },
   envDir: path.resolve(import.meta.dirname),
   root: path.resolve(import.meta.dirname, "client"),
+  base: "/tower_defense/",
   build: {
     outDir: path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
